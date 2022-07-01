8fdddd7e410708baf5aebda0b1cb93bf56fdfa27
wes-santos <weslleyalmerindo2@gmail.com>
2022-07-01 00:12:38 -0300

    Updates

---
+++ precache-manifest.8f9a0f8efae57761957957db25cb465d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb2ce5de4c596345f4921ee780aaad39",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "09938661b4add351bb26",
+    "url": "/trybewallet/static/css/main.7aae48d1.chunk.css"
+  },
+  {
+    "revision": "4c7a1267f99e242f1e9b",
+    "url": "/trybewallet/static/js/2.c9377961.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.c9377961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09938661b4add351bb26",
+    "url": "/trybewallet/static/js/main.5ce28854.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  },
+  {
+    "revision": "7adc7d912d608960529997e96ec31c09",
+    "url": "/trybewallet/static/media/editIcon.7adc7d91.svg"
+  },
+  {
+    "revision": "eea427274983c3c9edd4e3f9bb94b6ec",
+    "url": "/trybewallet/static/media/removeIcon.eea42727.svg"
+  }
+]);
