3ee0746dd590b61ef67b305b13313ab59ac77a1a
wes-santos <weslleyalmerindo2@gmail.com>
2022-07-01 00:07:13 -0300

    Updates

---
+++ precache-manifest.6dedcb3d8e434427449e1a78bccaa143.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b0ac959b6a93124e5b07a720f7ea72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f600205f09fd6f6530c9",
+    "url": "/static/css/main.7aae48d1.chunk.css"
+  },
+  {
+    "revision": "4c7a1267f99e242f1e9b",
+    "url": "/static/js/2.c9377961.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c9377961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f600205f09fd6f6530c9",
+    "url": "/static/js/main.e84285c0.chunk.js"
+  },
+  {
+    "revision": "fe617c2cb41aece9e721",
+    "url": "/static/js/runtime-main.e8df815d.js"
+  },
+  {
+    "revision": "7adc7d912d608960529997e96ec31c09",
+    "url": "/static/media/editIcon.7adc7d91.svg"
+  },
+  {
+    "revision": "eea427274983c3c9edd4e3f9bb94b6ec",
+    "url": "/static/media/removeIcon.eea42727.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/trybewallet/precache-manifest.8cec2aa4aa97380855cf86f2cc3c30aa.js"
+  "/precache-manifest.6dedcb3d8e434427449e1a78bccaa143.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trybewallet/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
